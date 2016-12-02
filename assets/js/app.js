82ada65b9f9c49776c36d440880ec8151ad43876
c00z <zcusimano@gmail.com>
2016-12-02 08:45:57 -0800

    Added jQuery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,8 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-  var links = $(".hwlink"); // warning sybmol for work
-  links.on("click", function handleClick(event){
+  $(".hwlink").on("click", function handleClick(event){
     event.preventDefault();
-    alert("Warning! I'm not smart. Do not copy");
+    alert("Warning! I'm not smart. Do not copy!!");
   });
 });
